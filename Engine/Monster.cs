6a868d76867eb8dd0a69c8df9b9229c36a07cac5
PAtomsk <131534215+PAtomsk@users.noreply.github.com>
2023-04-22 15:40:48 +0200

    Add files via upload

---
+++ Engine/Monster.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine
+{
+    public class Monster : LivingCreature
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int MaxDMG { get; set; }
+        public int RewardExp { get; set; }
+        public int RewardGold { get; set; }
+        public List<LootItem> LootTable { get; set; }
+
+        public Monster(int currentHP, int maxHP, int id, string name, int maxDMG, int rewardExp, int rewardGold):base(currentHP,maxHP)
+        {
+            ID = id;
+            Name = name;
+            MaxDMG = maxDMG;
+            RewardExp = rewardExp;
+            RewardGold = rewardGold;
+            LootTable = new List<LootItem>();
+        }
+
+    }
+}
